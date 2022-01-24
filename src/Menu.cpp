96c076e8a61dcdd015e0e201a1713d6b16d3a67b
Matilde <matilde.m.silva.21@gmail.com>
2022-01-24 14:03:00 +0000

    details

--- src/Menu.cpp
+++ src/Menu.cpp
@@ -5,10 +5,10 @@
 #include "Menu.h"
 #include <conio.h>
 
-void Menu::Menu(){}
+Menu::Menu(){}
 
 void Menu::showMenu() {
-    std::cout << "STCP APP" << endl;
+    std::cout << "STCP APP" << std::endl;
     std::cout << "Press the indicated below numbers to select what you want to do" << std::endl;
     //calculate the best path between two locations
     std::cout << "1 - Choose departure location (coordinates)" << std::endl;
@@ -17,13 +17,13 @@ void Menu::showMenu() {
     //need to verify if this path is valid (if it's in lines.csv)
     std::cout << "3 - Choose departure stop" << std::endl;
     std::cout << "4 - Choose destiny stop" << std::endl;
-    std::cout << "Now press the indicated below numbers to choose the type of path you want"
+    std::cout << "Now press the indicated below numbers to choose the type of path you want";
     std::cout << "5 - Pass through the minimum number of stops" << std::endl;
     std::cout << "6 - Minimum distance" << std::endl;
     std::cout << "7 - Minimum line changes" << std::endl;
     std::cout << "8 - Cheaper trip" << std::endl;
     //probably some more options, this is just a draft
-    }
+
 }
 
 void Menu::takeInput() {
@@ -45,7 +45,7 @@ void Menu::takeInput() {
             case '5':
                 if (departureStop.empty() || destinyStop.empty()) {
                     std::cout << "You can't choose the type of path before selecting both stops!" << std::endl;
-                    showMenu():
+                    showMenu();
                 }
                 else {
                     //provide user with this type of path
@@ -54,7 +54,7 @@ void Menu::takeInput() {
             case '6':
                 if (departureStop.empty() || destinyStop.empty()) {
                     std::cout << "You can't choose the type of path before selecting both stops!" << std::endl;
-                    showMenu():
+                    showMenu();
                 }
                 else {
                     //provide user with this type of path
@@ -63,7 +63,7 @@ void Menu::takeInput() {
             case '7':
                 if (departureStop.empty() || destinyStop.empty()) {
                     std::cout << "You can't choose the type of path before selecting both stops!" << std::endl;
-                    showMenu():
+                    showMenu();
                 }
                 else {
                     //provide user with this type of path
@@ -72,13 +72,12 @@ void Menu::takeInput() {
             case '8':
                 if (departureStop.empty() || destinyStop.empty()) {
                     std::cout << "You can't choose the type of path before selecting both stops!" << std::endl;
-                    showMenu():
+                    showMenu();
                 }
                 else {
                     //provide user with this type of path
                 }
                 break;
         }
-    }
 
 }
